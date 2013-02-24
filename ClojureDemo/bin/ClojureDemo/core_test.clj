36279d829d33b06115fe6507577e21012f1f9912
Harald Kuegel <hkuegel@googlemail.com>
2013-02-24 02:26:48 +0100

    added leiningen structure from eclipse

---
+++ ClojureDemo/bin/ClojureDemo/core_test.clj
@@ -0,0 +1,7 @@
+(ns ClojureDemo.core-test
+  (:use clojure.test
+        ClojureDemo.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
---
+++ ClojureDemo/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to ClojureDemo
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
